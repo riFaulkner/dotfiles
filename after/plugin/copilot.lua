d3cb1e71ba2eaf82c9d4900eac600e94db7a0845
richardfaulkner-qz <richard.faulkner@quizlet.com>
2023-06-27 16:35:11 -0600

    Adding additional copilot mapping

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,8 +1,9 @@
 -- vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-L>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 vim.g.copilot_assume_mapped = true
 
--- Set the copilot filetypes 
+-- Set the copilot filetypes
 vim.g.copilot_filetypes = {
   ["*"] = false,
   ["javascript"] = true,
