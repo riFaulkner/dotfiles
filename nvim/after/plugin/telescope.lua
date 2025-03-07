679a1a7b84a07af3f5685c955f41b0b7a3c62d43
Rick <rfaulkner@strava.com>
2025-03-07 11:56:17 -0700

    add go to impl shortcut leader-gi

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -17,7 +17,7 @@ pcall(require('telescope').load_extension, 'fzf')
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = 'Find recently opened files' })
 vim.keymap.set('n', '<leader>sb', require('telescope.builtin').buffers,
-  { desc = '[s]earch [buffers] Find existing buffers' })
+  { desc = '[s]earch [b]uffers Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
@@ -32,3 +32,4 @@ vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>gi', require('telescope.builtin').lsp_implementations, { desc = '[G]oto [I]mplementation' })
