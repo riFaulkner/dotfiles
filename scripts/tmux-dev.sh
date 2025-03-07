6a0f7c13a7ff5c347c721c4dd9f5845a61820d86
Rick <rfaulkner@strava.com>
2025-03-07 11:54:02 -0700

    Co-locate tmux scrips
    Tmux scripts are now in the same file, and differentiated as methods.
    This also required an update to my zshrc that I'll have to add to the
    startup script

---
+++ scripts/tmux-dev.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+t_init() {
+	SESSION="utils"
+	SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
+
+	if [ -z "$SESSIONEXISTS" ]; then
+	# Create the session 
+	tmux new-session -d -s $SESSION
+
+	# Create a pane for python rename it and start the python interpreter
+	tmux rename-window -t 1 "python"
+	tmux send-keys -t $SESSION:python 'python3' C-m
+
+	# Create a pane for the terminal, this should take up the majority of the screen width
+	tmux split-window -dh -l 80%
+	fi
+
+	tmux attach-session -t $SESSION:python
+}
+
+t_new() {
+	DIR=$(pwd)
+	SESSION=$(echo "$1" | xargs)
+	echo "Creating a new session named: " $SESSION
+
+	if ! tmux has-session -t "$SESSION" 2>/dev/null; then
+
+	tmux new-session -d -s "$SESSION" -c $DIR -n nvim
+
+	tmux send-keys -t "$SESSION:nvim" 'nvim .' C-m
+
+	tmux new-window -t $SESSION -n git
+	tmux send-keys -t $SESSION:git 'lg' C-m
+
+	tmux new-window -t $SESSION -n shell
+
+	fi
+
+	tmux attach-session -t $SESSION:nvim
+}
+
