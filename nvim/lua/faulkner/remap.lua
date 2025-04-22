7aaa9128979f34f6c96ae3504464c0c7e1dc53c4
Rick <rfaulkner@strava.com>
2025-04-22 08:19:04 -0600

    colocating telescope conf

--- nvim/lua/faulkner/remap.lua
+++ nvim/lua/faulkner/remap.lua
@@ -8,11 +8,6 @@ vim.api.nvim_set_keymap("n", "<C-l>", ":wincmd l<CR>", { silent = true })
 vim.api.nvim_set_keymap("n", "<leader>tn", ":tabn<CR>", { silent = true })
 vim.api.nvim_set_keymap("n", "<leader>tp", ":tabp<CR>", { silent = true })
 
-
--- Keymaps for better default experience
--- See `:help vim.keymap.set()`
-vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
-
 -- Remap for dealing with word wrap
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,8 +5,6 @@ return {
 	-- Fuzzy Finder Algorithm which requires local dependencies to be built.
 	{
 		"nvim-telescope/telescope-fzf-native.nvim",
-		-- NOTE: If you are having trouble with this installation,
-		--       refer to the README for telescope-fzf-native for more instructions.
 		build = "make",
 		cond = function()
 			return vim.fn.executable("make") == 1
@@ -16,6 +14,14 @@ return {
 		"nvim-telescope/telescope-ui-select.nvim",
 		config = function()
 			require("telescope").setup {
+				defaults = {
+					mappings = {
+						i = {
+							['<C-u>'] = false,
+							['<C-d>'] = false,
+						},
+					},
+				},
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown {
@@ -23,6 +29,40 @@ return {
 					}
 				}
 			}
+
+			local builtin = require("telescope.builtin")
+
+
+			-- Enable telescope fzf native, if installed
+			-- pcall(require('telescope').load_extension, 'fzf')
+
+			vim.keymap.set("n", "<leader>?", builtin.oldfiles, { desc = "[?] Find recently opened files" })
+
+			vim.keymap.set('n', '<leader>sb', builtin.buffers,
+				{ desc = '[s]earch [b]uffers Find existing buffers' })
+
+
+			vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Search [G]it [F]iles' })
+			vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+			vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+			vim.keymap.set('n', '<leader>sw', builtin.grep_string,
+				{ desc = '[S]earch current [W]ord' })
+			vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+			vim.keymap.set('n', '<leader>sd', builtin.diagnostics,
+				{ desc = '[S]earch [D]iagnostics' })
+			vim.keymap.set('n', '<leader>gi', builtin.lsp_implementations,
+				{ desc = '[G]oto [I]mplementation' })
+
+
+			-- See `:help telescope.builtin`
+			vim.keymap.set('n', '<leader>/', function()
+				-- You can pass additional configuration to telescope to change theme, layout, etc.
+				builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+					winblend = 10,
+					previewer = false,
+				})
+			end, { desc = '[/] Fuzzily search in current buffer' })
+
 			require("telescope").load_extension("ui-select")
 		end
 	}
